8bfa57e2ff93d5171475bacb83f7f7569bc1976a
chickenfan <35655087+chickenfan@users.noreply.github.com>
2021-02-13 14:27:33 +0000

    Update Friends.cpp

--- Friends.cpp
+++ Friends.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int secretNum = 7;
+    int guess = 0;
+    int guessCount = 0;
+    bool outofguesses = false;
+    int guesslimit = 5;
+
+    while(secretNum != guess &&  !outofguesses) {
+        if (guessCount < guesslimit) {
+
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        }
+        else {
+            outofguesses = true;
+        }
+    }
+ 
+    if (outofguesses) {
+        cout << "You lose because u suck ass bro";
+    }
+    else {
+        cout << "You win git gud";
+    }
+
+}
